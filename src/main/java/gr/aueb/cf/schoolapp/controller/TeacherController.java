0ee89892ba734f675567258d421a43f3e7ad4eb9
a8anassis <a8anassis@gmail.com>
2026-07-28 21:01:04 +0300

    feat(validator): implement TeacherInsertValidator for teacher insertion validation

--- src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -1,10 +1,15 @@
 package gr.aueb.cf.schoolapp.controller;
 
+import gr.aueb.cf.schoolapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.schoolapp.core.exceptions.EntityNotFoundException;
 import gr.aueb.cf.schoolapp.dto.RegionReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import gr.aueb.cf.schoolapp.service.IRegionService;
 import gr.aueb.cf.schoolapp.service.ITeacherService;
+import gr.aueb.cf.schoolapp.validator.TeacherInsertValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +31,7 @@ public class TeacherController {
 
     private final ITeacherService teacherService;
     private final IRegionService regionService;
-//    private final TeacherInsertValidator teacherInsertValidator;
+    private final TeacherInsertValidator teacherInsertValidator;
 
 //    @Autowired
 //    public TeacherController(ITeacherService teacherService, IRegionService regionService) {
@@ -46,7 +51,7 @@ public class TeacherController {
                                 BindingResult bindingResult, Model model,
                                 RedirectAttributes redirectAttributes) {
 
-//        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
+        teacherInsertValidator.validate(teacherInsertDTO, bindingResult);       // business rules
 
         if (bindingResult.hasErrors()) {
 //            model.addAttribute("regionsReadOnlyDTO", regions());
@@ -55,10 +60,24 @@ public class TeacherController {
 
         try {
             // save τον teacher
+            TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO);
             // επιστρέψει ένα success page
-        } catch () {
 
+            // PRG -- Post-Redirect-Get
+            redirectAttributes.addAttribute("teacherReadonlyDTO", teacherReadOnlyDTO);
+            return "redirect:/teachers/success";        // controller
+        } catch (EntityAlreadyExistsException | EntityInvalidArgumentException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "teacher-insert";
         }
+    }
+
+    @GetMapping("/success")
+    public String teacherInsertSuccess(Model model) {
+        if (!model.containsAttribute("teacherInsertDTO")) {     // ελέγχει το F5 - refresh
+            return "redirect:/teachers";
+        }
+        return "teacher-success";
     }
 
 
--- src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -1,10 +1,10 @@
 package gr.aueb.cf.schoolapp.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+// TODO Localization
 public record TeacherInsertDTO(
         @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό.")
         @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον δύο χαρακτήρες.")
